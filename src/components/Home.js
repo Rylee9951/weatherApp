94db9273eaa7c6a5a5da6b184f21074c702b28c4
Ryan Lee <djrlee51@gmail.com>
2020-06-24 09:13:20 -0700

    finished getting weather for a city

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,30 @@
 import React from "react"
 import { Card, Image, Dropdown } from "semantic-ui-react"
 import Sunny from "../assets/sunny.png"
+import ThunderStorm from "../assets/thunderstorm.png"
+import Cloudy from "../assets/cloudy.png"
+import Rainy from "../assets/rain.png"
+import Snow from "../assets/snow.png"
+
 import { useWeather } from "../redux/ducks/weather"
 
 export default (props) => {
-  const { states, getCities, cities, getWeather } = useWeather()
+  const {
+    states,
+    getCities,
+    cities,
+    get5DayWeather,
+    weather5days,
+  } = useWeather()
   function handleStatesChange(e, { value }) {
     getCities(value)
   }
   function handleCitiesChange(e, { value }) {
-    getWeather(value)
+    get5DayWeather(value)
   }
   return (
     <div>
+      <h3>Choose your State then City</h3>
       <Dropdown
         onChange={handleStatesChange}
         placeholder="Choose a State"
@@ -30,24 +42,38 @@ export default (props) => {
         search
         selection
         options={cities.map((item) => ({
-          key: item.rank,
-          text: item.city,
-          value: item.city,
+          key: item,
+          text: item,
+          value: item,
         }))}
       />
       <Card.Group>
-        <Card>
-          <Card.Content>
-            <Card.Header style={{ textAlign: "center" }}>Monday</Card.Header>
-            <Image src={Sunny} />
-          </Card.Content>
-          <Card.Content extra>
-            <div style={{ display: "flex", justifyContent: "space-around" }}>
-              <div>100&#xb0;</div>
-              <div>80&#xb0;</div>
-            </div>
-          </Card.Content>
-        </Card>
+        {weather5days.map((day) => (
+          <Card key={`dayOfWeek-${day.day}`}>
+            <Card.Content>
+              <Card.Header style={{ textAlign: "center" }}>
+                {day.day}
+              </Card.Header>
+              {day.weather === "Rain" ? (
+                <Image src={Rainy} />
+              ) : day.weather === "Clouds" ? (
+                <Image src={Cloudy} />
+              ) : day.weather === "Snow" ? (
+                <Image src={Snow} />
+              ) : day.weather === "ThunderStorm" ? (
+                <Image src={ThunderStorm} />
+              ) : (
+                <Image src={Sunny} />
+              )}
+            </Card.Content>
+            <Card.Content extra>
+              <div style={{ display: "flex", justifyContent: "space-around" }}>
+                <div>{day.minTemp}&#xb0;</div>
+                <div>{day.maxTemp}&#xb0;</div>
+              </div>
+            </Card.Content>
+          </Card>
+        ))}
       </Card.Group>
     </div>
   )
--- src/redux/ducks/weather/index.js
+++ src/redux/ducks/weather/index.js
@@ -42,14 +42,16 @@ function getStates() {
 function getTheCities(state) {
   return (dispatch) => {
     let cities = Usa.filter((item) => item.state === state)
+    let finalCities = cities.map((item) => item.city).sort()
+    console.log(finalCities)
     dispatch({
       type: GET_CITIES,
-      payload: cities,
+      payload: finalCities,
     })
   }
 }
 const apiKey = "17206ea6a26761548da827869b4b3237"
-function getWeatherNow(city) {
+function get5DayWeatherNow(city) {
   let newCity = city.toLowerCase()
   return (dispatch) => {
     axios
@@ -57,10 +59,106 @@ function getWeatherNow(city) {
         `http://api.openweathermap.org/data/2.5/forecast?q=${newCity}&appid=${apiKey}`
       )
       .then((resp) => {
-        console.log(resp)
+        const data = resp.data.list
+
+        let weather5day = data.filter((item) =>
+          item.dt_txt.includes("12:00:00")
+        )
+        let day1 = []
+        let day2 = []
+        let day3 = []
+        let day4 = []
+        let day5 = []
+        //looping thru to build day arrays
+        for (let i = 0; i < data.length; i++) {
+          if (i <= 7) {
+            day1.push(data[i])
+          } else if (i >= 8 && i <= 15) {
+            day2.push(data[i])
+          } else if (i >= 16 && i <= 23) {
+            day3.push(data[i])
+          } else if (i >= 24 && i <= 31) {
+            day4.push(data[i])
+          } else {
+            day5.push(data[i])
+          }
+        }
+        //getting just the temps
+        let day1Avg = day1.map((item) => item.main.temp)
+        let day2Avg = day2.map((item) => item.main.temp)
+        let day3Avg = day3.map((item) => item.main.temp)
+        let day4Avg = day4.map((item) => item.main.temp)
+        let day5Avg = day5.map((item) => item.main.temp)
+        //finding the max and min of temps
+        let day1maxAvgTotal = Math.max(...day1Avg)
+        let day1minAvgTotal = Math.min(...day1Avg)
+        let day2maxAvgTotal = Math.max(...day2Avg)
+        let day2minAvgTotal = Math.min(...day2Avg)
+        let day3maxAvgTotal = Math.max(...day3Avg)
+        let day3minAvgTotal = Math.min(...day3Avg)
+        let day4maxAvgTotal = Math.max(...day4Avg)
+        let day4minAvgTotal = Math.min(...day4Avg)
+        let day5maxAvgTotal = Math.max(...day5Avg)
+        let day5minAvgTotal = Math.min(...day5Avg)
+        //converting from Kelvin to  Fahrenheit
+        let day1maxAvgF = Math.round(((day1maxAvgTotal - 273.15) * 9) / 5 + 32)
+        let day1minAvgF = Math.round(((day1minAvgTotal - 273.15) * 9) / 5 + 32)
+        let day2maxAvgF = Math.round(((day2maxAvgTotal - 273.15) * 9) / 5 + 32)
+        let day2minAvgF = Math.round(((day2minAvgTotal - 273.15) * 9) / 5 + 32)
+        let day3maxAvgF = Math.round(((day3maxAvgTotal - 273.15) * 9) / 5 + 32)
+        let day3minAvgF = Math.round(((day3minAvgTotal - 273.15) * 9) / 5 + 32)
+        let day4maxAvgF = Math.round(((day4maxAvgTotal - 273.15) * 9) / 5 + 32)
+        let day4minAvgF = Math.round(((day4minAvgTotal - 273.15) * 9) / 5 + 32)
+        let day5maxAvgF = Math.round(((day5maxAvgTotal - 273.15) * 9) / 5 + 32)
+        let day5minAvgF = Math.round(((day5minAvgTotal - 273.15) * 9) / 5 + 32)
+        //Creating the day names from the date object
+        let days = weather5day.map((item, i) => {
+          const date = new Date(item.dt_txt)
+          let dayNumber = date.getDay()
+          let day = ""
+          dayNumber === 0
+            ? (day = "Sunday")
+            : dayNumber === 1
+            ? (day = "Monday")
+            : dayNumber === 2
+            ? (day = "Tuesday")
+            : dayNumber === 3
+            ? (day = "Wednesday")
+            : dayNumber === 4
+            ? (day = "Thursday")
+            : dayNumber === 5
+            ? (day = "Friday")
+            : (day = "Saturday")
+
+          return {
+            day,
+            weather: item.weather[0].main,
+            minTemp:
+              i === 0
+                ? day1minAvgF
+                : i === 1
+                ? day2minAvgF
+                : i === 2
+                ? day3minAvgF
+                : i === 3
+                ? day4minAvgF
+                : day5minAvgF,
+            maxTemp:
+              i === 0
+                ? day1maxAvgF
+                : i === 1
+                ? day2maxAvgF
+                : i === 2
+                ? day3maxAvgF
+                : i === 3
+                ? day4maxAvgF
+                : day5maxAvgF,
+          }
+        })
+
         dispatch({
           type: GET_WEATHER,
-          payload: resp.data.list,
+          payload: days,
         })
       })
   }
@@ -71,11 +169,13 @@ export function useWeather() {
   const dispatch = useDispatch()
   const states = useSelector(({ weatherReducer }) => weatherReducer.states)
   const cities = useSelector(({ weatherReducer }) => weatherReducer.cities)
-  const weather = useSelector(({ weatherReducer }) => weatherReducer.weather)
+  const weather5days = useSelector(
+    ({ weatherReducer }) => weatherReducer.weather
+  )
   const getCities = (state) => dispatch(getTheCities(state))
-  const getWeather = (city) => dispatch(getWeatherNow(city))
+  const get5DayWeather = (city) => dispatch(get5DayWeatherNow(city))
   useEffect(() => {
     dispatch(getStates())
   }, [dispatch])
-  return { states, getCities, cities, getWeather, weather }
+  return { states, getCities, cities, get5DayWeather, weather5days }
 }
